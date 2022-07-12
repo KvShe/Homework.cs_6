a96f3252e69a74796e6a905f289bc21b2c833df4
KvShe <zheshe@list.ru>
2022-07-12 11:25:46 +0300

    Final

---
+++ Exercise_41/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Write("Сколько чисел будешь вводить?\n");
+int size = 0;
+int[] array = new int[size];
+int count = 0;
+
+try
+{
+    size = EnterNumber(size);
+    array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        System.Console.WriteLine($"\nEnter number № {i + 1}");
+        array[i] = EnterNumber(array[i]);
+        if (array[i] > 0) count++;
+    }
+    Console.Write($"\n{count} numbers greater than zero\n ");
+}
+
+catch (OverflowException)
+{
+    Console.WriteLine("Судя по всему ты, что-то знаешь про отрицательный размер массива\nЕсли нет, то пожалуйста, не делай так больше");
+}
+
+int EnterNumber(int number)
+{
+    bool result;
+    do
+    {
+        Console.Write("Enter number: ");
+        result = int.TryParse(Console.ReadLine(), out number);
+    }
+    while (result == false);
+    return number;
+}
---
+++ Exercise_43/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.WriteLine("\nНапишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями:\n" +
+                    "y = b1 + k1 * x \n" +
+                    "y = b2 + k2 * x \n" +
+                    "Значения b1, k1, b2 и k2 задаются пользователем\n" +
+                    "b1 = 2, k1 = 5, b2 = 4, k2 = 9\n" +
+                    "--> (-0,5; -0,5)");
+string[] nameVariable = { "b1", "k1", "b2", "k2" };
+int size = 4;
+double[] variable = new double[size];
+for (int i = 0; i < size; i++)
+{
+    Console.Write($"\nEnter value {nameVariable[i]}\n");
+    variable[i] = EnterNumber(variable[i]);
+}
+double firstStraight = (variable[2] - variable[0]) / (variable[1] - variable[3]);
+double cesondStraigth = variable[0] + variable[1] * firstStraight;
+
+bool checkPoint  =  variable[0] == variable[1] &&
+                    variable[0] == variable[2] &&
+                    variable[0] == variable[3] &&
+                    variable[1] == variable[2] &&
+                    variable[1] == variable[3] &&
+                    variable[2] == variable[3];
+if (checkPoint) Console.WriteLine("\nКак я понял - это точка");
+else if (variable[1] == 0 &&
+         variable[3] == 0) Console.WriteLine("\nЭто вроде тоже точка");
+else if (variable[1] == variable[3]) Console.WriteLine("\nПрямые параллельны");
+else
+{
+    Console.WriteLine($"\n({firstStraight}, {cesondStraigth})");
+    Console.WriteLine("У меня какие-то проблемы с вещественными числами\nЯ минут 5 не мог понять, почему у меня через int не выводится -0,5");
+}
+
+double EnterNumber(double number)
+{
+    bool result;
+    do
+    {
+        Console.Write("Enter number: ");
+        result = double.TryParse(Console.ReadLine(), out number);
+    }
+    while (result == false);
+    return number;
+}
